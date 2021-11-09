6a507e8077594789c3ac65cc56e479dbfdd51210
SajadRahimi2 <sajadrahimi0101@gmail.com>
2021-11-09 16:52:49 +0330

    Custom Switch designed

--- lib/views/screens/profile_setting_screen.dart
+++ lib/views/screens/profile_setting_screen.dart
@@ -5,27 +5,7 @@ class CustomSwitch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        GestureDetector(
-          onLongPressEnd: (details) {
-            print("ended");
-          },
-          child: Container(
-            color: Colors.cyan,
-            width: 50,
-            height: 30,
-          ),
-        ),
-        InkWell(
-            onHover: (value) {
-              print("Hover");
-            },
-            child: Container(
-              color: Colors.red,
-              width: 10,
-              height: 50,
-            ))
-      ],
+      children: [],
     );
   }
 }
